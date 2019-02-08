9b28eecf6ecc67e023847ed19ae4f02f84904033
bogdanfacaianu <bogdy.facaianu@gmail.com>
2019-02-08 13:28:42 +0000

    not sure if this thing should terminate if in cmd with arguments mode

--- src/main/java/com/bookings/rides/RidesApplication.java
+++ src/main/java/com/bookings/rides/RidesApplication.java
@@ -1,13 +1,40 @@
 package com.bookings.rides;
 
+import com.bookings.rides.entity.Car;
+import com.bookings.rides.entity.CarType;
+import com.bookings.rides.service.SupplierService;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RidesApplication {
+public class RidesApplication implements CommandLineRunner {
+
+    @Autowired
+    private SupplierService supplierService;
+
+    private static final int DEFAULT_MAXIMUM_PASSENGERS = CarType.MINIBUS.getSeats();
 
     public static void main(String[] args) {
         SpringApplication.run(RidesApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        if (args.length > 0) {
+            String latitude = args[0];
+            String longitude = args[1];
+            int maximumPassengers = args.length == 3 ? Integer.parseInt(args[2]) : DEFAULT_MAXIMUM_PASSENGERS;
+
+            List<Car> response = supplierService.getSupplierResponses(latitude, longitude, maximumPassengers);
+            String offers = supplierService.convertToJson(response);
+
+            System.out.println(offers);
+        }
     }
 
 }
